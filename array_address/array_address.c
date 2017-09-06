efd7d4b133cadc4fc0abbc4b0a0590d524b34858
lzhou <trunkMode20@126.com>
2017-09-06 05:57:13 -0600

    Update array address test program

--- array_address/array_address.c
+++ array_address/array_address.c
@@ -5,5 +5,6 @@ int main()
  	char a[6] = {0x00};
 	char *p = &a[0];
 	char *q = &p;
-	printf("&a = %p, &a[0] = %p, &&a[0] = %p\n", &a, &a[0], q);
+	printf("&a = %p, &a[0] = %p, &&a[0] = %p\n", &a, &a[0], &&a[0]]);
+	printf("p = %p, q = %p");
 }
