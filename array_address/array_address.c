98b72506287086084747fe7c5d83a23f3e96e9c3
lzhou <lzhou@sonicwall.com>
2017-09-06 22:23:28 -0600

    alan add some lines in array_address.c

--- array_address/array_address.c
+++ array_address/array_address.c
@@ -5,6 +5,9 @@ int main()
  	char a[6] = {0x00};
 	char *p = &a[0];
 	char *q = &p;
+	
 	printf("&a = %p, &a[0] = %p, &&a[0] = %p\n", &a, &a[0], &&a[0]]);
 	printf("p = %p, q = %p");
+	
+	printf("alan update\n");
 }
