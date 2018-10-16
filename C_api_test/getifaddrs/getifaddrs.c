6abcc1df1b9c0f5c324d0974e6d2ff353a6e1e2e
root <root@localhost.localdomain>
2018-10-16 02:30:17 -0600

    getifaddrs: add the usage example of getifaddrs

--- C_api_test/getifaddrs/getifaddrs.c
+++ C_api_test/getifaddrs/getifaddrs.c
@@ -25,6 +25,7 @@ int main()
     int af = AF_INET;
     char addr_str[32];
 	struct ifaddrs *ifap = NULL, *tmp_ifap;
+	struct sockaddr_in *sk_addr;
 
 	if (getifaddrs(&ifap) < 0) {
 		perror("getifaddrs failed:");
@@ -35,17 +36,36 @@ int main()
 	while (tmp_ifap) {
 		printf("ifa_name: %s\n", tmp_ifap->ifa_name);
 		printf("ifa_flags: %x\n", tmp_ifap->ifa_flags);
-	
-	    if (inet_ntop(tmp_ifap->ifa_addr->sa_family, tmp_ifap->ifa_addr, addr_str, sizeof(addr_str))) {
-	        printf("addr: %s\n", addr_str);
+		
+		sk_addr = (struct sockaddr_in *) tmp_ifap->ifa_addr;
+	    printf("ifa_addr: %0x\n", ntohl(sk_addr->sin_addr.s_addr));
+	    
+	    if (tmp_ifap->ifa_addr) {
+	        if (tmp_ifap->ifa_addr->sa_family == AF_INET && 
+	           inet_ntop(AF_INET, &(((struct sockaddr_in *)tmp_ifap->ifa_addr)->sin_addr), addr_str, sizeof(addr_str))) {
+	            printf("addr: %s\n", addr_str);
+	        } else if (tmp_ifap->ifa_addr->sa_family == AF_INET6 &&
+	            inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)tmp_ifap->ifa_addr)->sin6_addr), addr_str, sizeof(addr_str))) {
+	            printf("addr: %s\n", addr_str);
+	        }
 	    }
 	    
-	    if (tmp_ifap->ifa_netmask && inet_ntop(tmp_ifap->ifa_netmask->sa_family, tmp_ifap->ifa_netmask, addr_str, sizeof(addr_str))) {
-	        printf("ifa_netmask: %s\n", addr_str);
+	    if (tmp_ifap->ifa_netmask) {
+	        if (tmp_ifap->ifa_netmask->sa_family == AF_INET &&
+	            inet_ntop(AF_INET, &(((struct sockaddr_in *)tmp_ifap->ifa_netmask)->sin_addr), addr_str, sizeof(addr_str))) {
+	            printf("netmask: %s\n", addr_str);
+	        } else if (tmp_ifap->ifa_addr->sa_family == AF_INET6 &&
+	            inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)tmp_ifap->ifa_netmask)->sin6_addr), addr_str, sizeof(addr_str))) {
+	            printf("netmask: %s\n", addr_str);
+	        }
+	        
 	    }
+
         printf("\n\n");
         tmp_ifap = tmp_ifap->ifa_next;
 	}
+	freeifaddrs(tmp_ifap);
+
 }
 
 
--- array_address/array_address.c
+++ array_address/array_address.c
@@ -1,14 +1,42 @@
+#include <mcheck.h>
 #include <unistd.h>
+
+int two_dimension_array_test()
+{
+	int i,j;
+	char b[3][10];
+
+	memset(b, 0x8, 3*10);
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 10; j++) {
+			printf("b[%d][%d] = %d\n", i, j, b[i][j]);
+		}
+	}
+}
+
+int crash_test()
+{
+	char *p = NULL;
+	p = malloc(100);
+	*p= 0x88;
+	return 0;
+}
 
 int main()
 {
  	char a[6] = {0x00};
 	char *p = &a[0];
 	char *q = &p;
+
+	mtrace();
 	
-	printf("&a = %p, &a[0] = %p, &&a[0] = %p\n", &a, &a[0], &&a[0]]);
-	printf("p = %p, q = %p");
+	printf("&a = %p, &a[0] = %p, &&a[0] = %p\n", &a, &a[0], &a);
+	printf("p = %p, q = %p\n");
+
+	two_dimension_array_test();
 	
+	crash_test();
+
 	printf("alan update\n");
 	printf("alan add 20171013\n");
 
